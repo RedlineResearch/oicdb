089236b8ed7ec28dfe3dd9045094a1ab1c378520
Karl Cronburg <karl.cronburg@gmail.com>
2014-04-09 12:39:51 -0400

    todo

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import pycparser
 from pycparser import parse_file
 import pickle
 import os
+import glob
 
 def grab_main(fn, cpp_path='cpp'):
   ast = parse_file(fn, use_cpp=True, cpp_path=cpp_path, \
@@ -15,10 +16,15 @@ def grab_main(fn, cpp_path='cpp'):
   print "No main() found in '%s'"%fn
   return None
 
+for fn in glob.glob("rubrics/*.c"):
+  pickle.dump(grab_main(fn), open(fn[:-2]+".pkl", 'w'))
+
+"""
 pickle.dump(grab_main("rubrics/setup.c"), open("rubrics/setup.pkl", 'w'))
 pickle.dump(grab_main("rubrics/fncn.c"), open("rubrics/fncn.pkl", 'w'))
 pickle.dump(grab_main("rubrics/var.c"), open("rubrics/var.pkl", 'w'))
 pickle.dump(grab_main("rubrics/return.c"), open("rubrics/return.pkl", 'w'))
 pickle.dump(grab_main("rubrics/param.c"), open("rubrics/param.pkl", 'w'))
 pickle.dump(grab_main("rubrics/unary.c"), open("rubrics/unary.pkl", 'w'))
+"""
 
